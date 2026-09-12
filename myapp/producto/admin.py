dce13f91416fe57f20b002f4965ee1b95eea47b6
Damian Rojas <rojasdamianc@gmail.com>
2026-09-11 23:49:38 -0300

    Se agrega el Proveedor al Admin

--- myapp/producto/admin.py
+++ myapp/producto/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 #Models
-from .models import Categoria, Producto
+from .models import Categoria, Producto, Proveedor
 
 
 # Register your models here.
@@ -10,7 +10,7 @@ from .models import Categoria, Producto
 # Personalizacion Administrador de productos
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
-    list_display=('id','nombre','categoria','talle','color','imagen','precio')
+    list_display=('id','nombre','categoria','id_proveedor','talle','color','imagen','precio')
     list_display_links=('nombre',)
     list_filter=('categoria','nombre',)
     search_fields=('nombre',)
@@ -24,3 +24,17 @@ class CategoriaAdmin(admin.ModelAdmin):
     list_display = ('id', 'descripcion')
     list_display_links = ('descripcion',)
     search_fields = ('descripcion',)
+
+
+@admin.register(Proveedor)
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'nombre',
+        'calle_nombre',
+        'calle_numero',
+        'localidad',
+        'telefono',
+    )
+    list_display_links = ('nombre',)
+    search_fields = ('nombre', 'calle_nombre', 'localidad')
