a88bd12f9696cdeeadfb14c03e550c4d1df8aece
Damian Rojas <rojasdamianc@gmail.com>
2026-09-11 23:49:58 -0300

    Se agrega el modelo de la clase Proveedor

--- myapp/producto/models.py
+++ myapp/producto/models.py
@@ -18,13 +18,38 @@ class Categoria(models.Model):
         verbose_name_plural = 'Categorías'
 
 
+class Proveedor(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    calle_nombre = models.CharField(max_length=100)
+    calle_numero = models.IntegerField()
+    localidad = models.CharField(max_length=100)
+    telefono = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        db_table = 'proveedor'
+        verbose_name = 'Proveedor'
+        verbose_name_plural = 'Proveedores'
+
+
 class Producto(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100, verbose_name='Nombre Producto')
     desc = RichTextField(verbose_name='Descripción')
     precio = models.FloatField()
     precio_costo = models.FloatField(null=True, blank=True)
-    id_proveedor = models.IntegerField(null=True, blank=True)
+    id_proveedor = models.ForeignKey(
+        Proveedor,
+        on_delete=models.PROTECT,
+        related_name='productos',
+        db_column='id_proveedor',
+        null=True,
+        blank=True,
+        verbose_name='Proveedor',
+    )
     talle = models.JSONField(default=list, blank=True)
     color = models.JSONField(default=list, blank=True)
     categoria = models.ForeignKey(
