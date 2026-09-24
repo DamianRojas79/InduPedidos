36f25d87b8ed48ec670a762f0e307f7e286d81b3
Damian Rojas <rojasdamianc@gmail.com>
2026-09-23 22:34:12 -0300

    Se agrega ruta para crear opciones de compra

--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -38,6 +38,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('mis-pedidos/', pedidos.mis_pedidos, name='mis_pedidos'),
+    path('pedidos/<int:linea_id>/opciones/agregar/', pedidos.agregar_opcion, name='agregar_opcion'),
     path('pedidos/agregar/', pedidos.agregar_pedido, name='agregar_pedido'),
     path('pedidos/<int:linea_id>/modificar/', pedidos.modificar_pedido, name='modificar_pedido'),
     path('pedidos/<int:linea_id>/eliminar/', pedidos.eliminar_pedido, name='eliminar_pedido'),
